b0599b1b5b43b7091133bc2e72d80ea93314db39
Ambar Gonzalez <49761975+Ambarpg80@users.noreply.github.com>
2024-09-11 12:13:26 -0400

    added solution

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,17 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(names):
+    for name in names:
+        print(f"{names.index(name)+1}. {name}")
+    
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(planet_call):
+     title_case = [f"{call.title()}!" for call in planet_call]
+     return title_case
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(calls):
+    over_four = [len(call) > 4 for call in calls]
+    return any(over_four)
+        
 
-def find_the_cheese():
-    pass
+def find_the_cheese(ingredients):
+    if ("cheddar" or "gouda" or "camembert") in ingredients:
+        return ("cheddar" or "gouda" or "camembert")
